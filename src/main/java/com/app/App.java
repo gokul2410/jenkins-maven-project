ebdc5ee0d74856b94d6b341cd216fc8168383e52
gokul2410 <gokulakrishnanv7@gmail.com>
2025-12-07 19:48:09 +0530

    commiting the first jenkins maven project

---
+++ src/main/java/com/app/App.java
@@ -0,0 +1,13 @@
+package com.example;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("--------------------------------------------------");
+        System.out.println("   SUCCESS! The Live Project is Running.          ");
+        System.out.println(" Jenkins has successfully built maven java project");
+        System.out.println("--------------------------------------------------");
+    }
+}
